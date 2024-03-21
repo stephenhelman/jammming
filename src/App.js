801ee9f2a2fcb73962e6594a56c1c10677d31d50
Stephen Helman <stephenhelman18@gmail.com>
2024-03-20 21:15:04 -0600

    Skeleton Structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        Ja<span>mmm</span>ing
       </header>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type='text' placeholder='Search for a song' />
+        <button type='submit'>Search</button>
+      </form>
+      <div>
+        <h2>Results</h2>
+        <ul>
+          <li>Song Title</li>
+        </ul>
+      </div>
+      <form>
+        <input type='text' placeholder='Name your Playlist' />
+        <ul>
+          <li>Song title</li>
+        </ul>
+        <button type='submit'>Save to spotify</button>
+      </form>
     </div>
   );
 }
